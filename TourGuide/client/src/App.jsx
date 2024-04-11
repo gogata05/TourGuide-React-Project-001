7f045e46a76aa780fb297dfab96d725b2dfe7f5d
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-04-12 02:58:03 +0300

    front-end added

---
+++ TourGuide/client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+
+    return (
+        <div className="App">
+        <h1>Hello</h1>
+      </div>
+    )
+}
+
+export default App;
