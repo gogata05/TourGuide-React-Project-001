e3eda8e6a074850eced20311e619965e78486d67
Georgi Markov <gogata1905@gmail.com>
2024-07-09 06:08:39 +0300

    add deployed hostname

--- TourGuide/client/src/services/requester.js
+++ TourGuide/client/src/services/requester.js
@@ -1,64 +1,64 @@
-const hostname = 'http://localhost:3000';
+const hostname = "https://tour-guide-react-project-001-ama7.vercel.app/";
 
 async function request(url, option) {
+  // try {
 
-    
-    // try {
+  const response = await fetch(hostname + url, option);
 
-        const response = await fetch(hostname + url, option);
+  if (response.ok !== true) {
+    const err = await response.json();
+    throw new Error(err.message);
+  }
 
-        if (response.ok !== true) {
-            const err = await response.json();
-            throw new Error(err.message);
-        }
+  if (response.status === 204) {
+    return response;
+  } else if (
+    response.headers.has("content-type") &&
+    response.headers.get("content-type").includes("application/json")
+  ) {
+    return response.json();
+  } else {
+    return response.text();
+  }
 
-        if (response.status === 204) {
-            return response;
-        } else if (response.headers.has('content-type') && response.headers.get('content-type').includes('application/json')) {
-            return response.json();
-        } else {
-            return response.text();
-        }
-
-    // } catch (error) {
-    //     // alert(error.message);
-    //     throw error;
-    // }
+  // } catch (error) {
+  //     // alert(error.message);
+  //     throw error;
+  // }
 }
 
-function createOptions(method = 'GET', data) {
+function createOptions(method = "GET", data) {
+  const option = {
+    method,
+    headers: {},
+  };
 
-    const option = {
-        method,
-        headers: {}
-    }
+  // const userData = JSON.parse(localStorage.getItem('userData'));
+  // const token = userData.authToken;
 
-    // const userData = JSON.parse(localStorage.getItem('userData'));
-    // const token = userData.authToken;
+  //     if(token){
+  //         option.headers['Authorization'] = `Bearer ${token}`
+  //     }
 
-    //     if(token){
-    //         option.headers['Authorization'] = `Bearer ${token}`
-    //     }
-
-    if (data !== undefined) {
-        option.headers['Content-Type'] = 'Application/json';
-        option.body = JSON.stringify(data);
-    }
-    return option;
+  if (data !== undefined) {
+    option.headers["Content-Type"] = "Application/json";
+    option.body = JSON.stringify(data);
+  }
+  return option;
 }
 
 export async function get(url) {
-    return request(url, createOptions());
+  return request(url, createOptions());
 }
 
 export async function post(url, data) {
-    return request(url, createOptions('post', data));
+  return request(url, createOptions("post", data));
 }
 
 export async function put(url, data) {
-    return request(url, createOptions('put', data));
+  return request(url, createOptions("put", data));
 }
 
 export async function del(url) {
-    return request(url, createOptions('delete'));
+  return request(url, createOptions("delete"));
 }
