a8826a1015a8556e5f7c02095ffc3c3bff235b5c
Georgi Markov <gogata1905@gmail.com>
2024-07-09 21:26:56 +0300

    comment

--- TourGuide/client/src/services/requester.js
+++ TourGuide/client/src/services/requester.js
@@ -1,28 +1,30 @@
 const hostname = "https://tourguide-react-project-001.onrender.com";
 
 async function request(url, option) {
-  try {
-    const response = await fetch(hostname + url, option);
+  // try {
 
-    if (response.ok !== true) {
-      const err = await response.json();
-      throw new Error(err.message);
-    }
+  const response = await fetch(hostname + url, option);
 
-    if (response.status === 204) {
-      return response;
-    } else if (
-      response.headers.has("content-type") &&
-      response.headers.get("content-type").includes("application/json")
-    ) {
-      return response.json();
-    } else {
-      return response.text();
-    }
-  } catch (error) {
-    alert(error.message);
-    throw error;
+  if (response.ok !== true) {
+    const err = await response.json();
+    throw new Error(err.message);
   }
+
+  if (response.status === 204) {
+    return response;
+  } else if (
+    response.headers.has("content-type") &&
+    response.headers.get("content-type").includes("application/json")
+  ) {
+    return response.json();
+  } else {
+    return response.text();
+  }
+
+  // } catch (error) {
+  //     // alert(error.message);
+  //     throw error;
+  // }
 }
 
 function createOptions(method = "GET", data) {
@@ -31,12 +33,12 @@ function createOptions(method = "GET", data) {
     headers: {},
   };
 
-  const userData = JSON.parse(localStorage.getItem("userData"));
-  const token = userData.authToken;
+  // const userData = JSON.parse(localStorage.getItem('userData'));
+  // const token = userData.authToken;
 
-  if (token) {
-    option.headers["Authorization"] = `Bearer ${token}`;
-  }
+  //     if(token){
+  //         option.headers['Authorization'] = `Bearer ${token}`
+  //     }
 
   if (data !== undefined) {
     option.headers["Content-Type"] = "Application/json";
@@ -44,19 +46,15 @@ function createOptions(method = "GET", data) {
   }
   return option;
 }
-
 export async function get(url) {
   return request(url, createOptions());
 }
-
 export async function post(url, data) {
   return request(url, createOptions("post", data));
 }
-
 export async function put(url, data) {
   return request(url, createOptions("put", data));
 }
-
 export async function del(url) {
   return request(url, createOptions("delete"));
 }
