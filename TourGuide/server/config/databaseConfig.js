2abd13415c098e982b24c9cb16582ee1a45aa950
Georgi Markov <gogata1905@gmail.com>
2024-07-10 01:44:46 +0300

    database config

--- TourGuide/server/config/databaseConfig.js
+++ TourGuide/server/config/databaseConfig.js
@@ -3,10 +3,6 @@ const { DB_CONNECTION_URL } = require('./env');
 
 exports.initDB = () => {
   mongoose.connection.on('open', () => console.log('Database is connected!'));
-  mongoose
-    .connect(DB_CONNECTION_URL, {
-      useNewUrlParser: true, //del
-      useUnifiedTopology: true //del
-    })
-    .catch(err => console.error('Failed to connect to MongoDB', err));
+  mongoose.connection.on('error', err => console.error('Database connection error:', err));
+  mongoose.connect(DB_CONNECTION_URL).catch(err => console.error('Failed to connect to MongoDB', err));
 };
