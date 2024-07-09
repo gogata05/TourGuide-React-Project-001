78ec132b89f742128f4f0492df65cdc90f0a3b3c
Georgi Markov <gogata1905@gmail.com>
2024-07-09 09:26:48 +0300

    mongo cluster

--- TourGuide/server/config/env.js
+++ TourGuide/server/config/env.js
@@ -1,8 +1,8 @@
 exports.PORT = process.env.PORT || 3000;
 const dbName = "TourGuide5";
-exports.DB_CONNECTION_URL = `mongodb://127.0.0.1:27017/${dbName}`;
+// exports.DB_CONNECTION_URL = `mongodb://127.0.0.1:27017/${dbName}`;
 
-// exports.DB_CONNECTION_URL =
-//   "mongodb+srv://gogata1905:<wOKKqmbHK271SdkD>@cluster0-tourguide2.atkegg8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TourGuide2";
+exports.DB_CONNECTION_URL =
+  "mongodb+srv://gogata1905:<wOKKqmbHK271SdkD>@cluster0-tourguide2.atkegg8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-TourGuide2";
 exports.SECRET = process.env.SECRET || "djas-jio2323-9887dasn23f321-dsdas";
 exports.SALT_ROUNDS = process.env.SALT_ROUNDS || 10;
